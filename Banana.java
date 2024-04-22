21b3f4ce6fd40293debe369ab5139f0fc9c6fdcb
Maksim59 <max.isayenka@gmail.com>
2024-04-22 19:29:54 -0400

    added sadface

--- Banana.java
+++ Banana.java
@@ -10,6 +10,12 @@ public class Banana extends Actor
         {
             resetBanana();
         }
+        if(isTouching(Hero.class))
+        {
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300,200);
+            getWorld().removeObject(this);
+        }
     }
     public void resetBanana()
     {
