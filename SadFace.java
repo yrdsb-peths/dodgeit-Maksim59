15a4a4b4d2f11261b18baee1604a9b1f4171ce7c
Maksim59 <max.isayenka@gmail.com>
2024-04-25 21:33:39 -0400

    found a mistake in sadface

--- SadFace.java
+++ SadFace.java
@@ -12,8 +12,5 @@ public class SadFace extends Actor
      * Act - do whatever the SadFace wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
-        
-    }
+    
 }
