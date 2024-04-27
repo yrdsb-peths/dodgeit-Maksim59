b226ddf39aeb94fdc1d7295ba98efd84f852ce0b
Maksim59 <max.isayenka@gmail.com>
2024-04-26 22:41:08 -0400

    changed size of world

--- MyWorld.java
+++ MyWorld.java
@@ -7,7 +7,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 600, 1); 
         Hero hero = new Hero();
         addObject(hero,50,100);
         showText("DODGE IT", 300, 20);
